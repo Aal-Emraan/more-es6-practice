846f354745e4e2e83c7a32e5bf8484559a55b198
Aal Emraan <developer.aalemraan@gmail.com>
2021-08-24 17:02:18 +0600

    class learned

--- class.js
+++ class.js
@@ -6,11 +6,30 @@ class Support{
         this.name = name;
         this.address = address;
     }
-    state(){
-        return `${this.name} is a ${this.designation} in our company. He is from ${this.address}.`
+    state(game){
+        return `${this.name} is a ${this.designation} in our company. He is from ${this.address}. He likes to play ${game}.`
+    }
+}
+
+class Post extends Support{
+    post;
+    constructor(name,address,post){
+        super(name,address);
+        this.post = post;
+    }
+    work(){
+        console.log(`${this.name} is ${this.post} of our company. He comes from the most beautifull city of bangladesh which is ${this.address}.`)
     }
 }
 
 const aal = new Support('Aal Emraan', 'Sylhet');
 console.log(aal);
 console.log(aal.state());
+
+const nadir = new Support('Nadir Ahmed', 'Dhaka');
+console.log(nadir);
+console.log(nadir.state('Football'));
+
+const aalEmraan = new Post('Aal Emraan','Sylhet','Chairman');
+console.log(aalEmraan);
+aalEmraan.work();
